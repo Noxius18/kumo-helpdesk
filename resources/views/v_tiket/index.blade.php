cb5565bb9c72ecd7fd3b77c3653edda19837f823
Noxius18 <akagiadam@gmail.com>
2024-12-11 17:04:58 +0700

    feat: buat catatan teknisi

--- resources/views/v_tiket/index.blade.php
+++ resources/views/v_tiket/index.blade.php
@@ -3,7 +3,9 @@
     @if (Auth::check())
         @if (Auth::user()->role->role === 'Admin')
             @include('layouts.index-tiket.admin')
-        @elseif(Auth::user()->role->role === 'Karyawan')
+        @elseif (Auth::user()->role->role === 'Teknisi')
+            @include('layouts.index-tiket.teknisi')
+        @elseif (Auth::user()->role->role === 'Karyawan')
             @include('layouts.index-tiket.karyawan')
         @endif
     @endif
