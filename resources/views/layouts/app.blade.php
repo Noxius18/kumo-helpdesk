27ee798e31195771130e01c65488691fb9b94e05
Noxius18 <akagiadam@gmail.com>
2024-12-11 19:12:07 +0700

    feat: Tambah dan cetak laporan tiket

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -91,7 +91,7 @@
                             <li><a href="{{ route('dashboard.admin.data-teknisi') }}" class="block px-4 py-2 hover:bg-kumoBlue-300 rounded flex items-center"><i class="fas fa-solid fa-chalkboard-user mr-2"></i>Teknisi</a></li>
                           </ul>
                         </li>
-                        <li><a href="#" class="block px-4 py-2 hover:bg-kumoBlue-300 rounded flex items-center"><i class="fas fa-chart-bar mr-2"></i>Reports</a></li>
+                        <li><a href="{{ route('admin.laporan') }}" class="block px-4 py-2 hover:bg-kumoBlue-300 rounded flex items-center"><i class="fas fa-chart-bar mr-2"></i>Laporan Tiket</a></li>
                     @elseif (Auth::user()->role->role === 'Teknisi')
                         <li><a href="{{ route('dashboard.teknisi') }}" class="block px-4 py-2 hover:bg-kumoBlue-300 rounded flex items-center"><i class="fas fa-th-large mr-2"></i>Dashboard</a></li>
                         <li><a href="{{ route('teknisi.list-tiket') }}" class="block px-4 py-2 hover:bg-kumoBlue-300 rounded flex items-center"><i class="fas fa-list mr-2"></i>List Tiket</a></li>
